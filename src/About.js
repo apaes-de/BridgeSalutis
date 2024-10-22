2a12269c5941aab701bb36ac0be3ed468c472ce4
Toni_Bispo <tonicrespo1998@gmail.com>
2024-10-22 17:53:49 +0100

    2º

--- src/About.js
+++ src/About.js
@@ -5,20 +5,29 @@ function About() {
     <section className="about" id="sobre-nos">
       <div className="about-card">
       <h2>Sobre Nós</h2>
-        <p>
-        A Bridge Salutis nasceu alicerçada na vontade de trazer algo novo e inovador para o setor da saúde em Portugal, oferecendo a empresas e pequenos negócios de Enfermagem a capacidade de crescerem e de se desenvolverem de forma sólida e competitiva. 
-        <p>
-        A nossa missão é ser a ponte, o elo de conexão entre empresas inovadoras e talentosas e o vasto mercado nacional e internacional. Permitimos que essas empresas alcancem o seu potencial máximo, oferecendo soluções avançadas que contribuam significativamente para a melhoria da saúde em todo o país.
-        Reconhecemos que muitas startups e pequenas empresas de Enfermagem enfrentam dificuldades que limitam o seu crescimento e desenvolvimento, como a falta de recursos tecnológicos e financeiros, acesso a investimentos estratégicos, conhecimento em questões burocráticas e legislativas, entre outros desafios. A Bridge Salutis atua com o propósito de facilitar o acesso a redes de investidores, oferecendo consultoria tecno-financeira e apoio especializado na transformação digital, preparando as empresas para a implementação de novas tecnologias no setor da saúde.
-        </p>
-        A nossa visão espelha o desejo de que Portugal acompanhe a evolução global nos cuidados de saúde, permitindo que os cidadãos tenham acesso a tratamentos e serviços mais avançados e eficazes. Ao apoiar empresas na área de Enfermagem com grande potencial de crescimento e as ferramentas adequadas, contribuímos para uma sociedade onde a saúde de excelência será uma realidade acessível a todos.
-        Na Bridge Salutis, os nossos valores fundamentais são a inovação, transparência, a proatividade, humildade e o respeito.
-        A proatividade é central para a nossa abordagem: estamos sempre dois passos à frente, antecipando necessidades e criando soluções antes que os problemas surjam. Agir de forma rápida e eficiente é essencial para garantir o sucesso dos nossos clientes.
-        A transparência e a humildade mantêm-nos conectados às reais necessidades dos nossos parceiros e clientes. Embora sejamos especialistas nas nossas áreas, acreditamos que há sempre espaço para aprender, ouvir e evoluir, respeitando as opiniões e experiências de todos.
-        Valorizamos profundamente o direito à liberdade de consciência, reconhecendo que o respeito pela diversidade de opiniões e valores é crucial para criar um ambiente de trabalho justo e inclusivo.
-        Estamos comprometidos em oferecer soluções personalizadas, garantindo que cada cliente tenha acesso aos melhores recursos para o seu crescimento. Mantemos também um compromisso contínuo com o desenvolvimento e adaptação às mudanças no mercado, pois acreditamos que a evolução constante é essencial para assegurar que Portugal esteja na linha da frente dos avanços tecnológicos e de saúde.
-        Estamos aqui para construir as pontes essenciais e imprescindíveis, que permitirão que  empresas e StartUps de Enfermagem prosperem, assegurando que o futuro da saúde seja brilhante, inovador e acessível a todos.
-        </p>
+       
+      <p>
+      A <strong>Bridge Salutis</strong> nasceu alicerçada na vontade de trazer algo novo e inovador para o setor da saúde em Portugal, oferecendo a <strong>empresas e pequenos negócios de Enfermagema</strong> a capacidade de crescerem e de se desenvolverem de forma sólida e competitiva. 
+      </p>
+      <p>
+      A <strong>nossa missão</strong> é ser a ponte, o elo de conexão entre empresas inovadoras e talentosas e o vasto mercado nacional e internacional. Permitimos que essas empresas alcancem o seu potencial máximo, oferecendo soluções avançadas que contribuam significativamente para a melhoria da saúde em todo o país.
+      Reconhecemos que muitas startups e pequenas empresas de Enfermagem enfrentam dificuldades que limitam o seu crescimento e desenvolvimento, como a falta de recursos tecnológicos e financeiros, acesso a investimentos estratégicos, conhecimento em questões burocráticas e legislativas, entre outros desafios.
+      A <strong>Bridge Salutis</strong> atua com o propósito de facilitar o acesso a redes de investidores, oferecendo <strong>consultoria tecno-financeira</strong> e apoio especializado na <strong>transformação digital</strong>, preparando as empresas para a implementação de novas tecnologias no setor da saúde.
+      </p>
+      <p>
+      A <strong>nossa visão </strong>espelha o desejo de que Portugal acompanhe a evolução global nos cuidados de saúde, permitindo que os cidadãos tenham acesso a tratamentos e serviços mais avançados e eficazes. Ao apoiar empresas na área de Enfermagem com grande potencial de crescimento e as ferramentas adequadas, contribuímos para uma sociedade onde a saúde de excelência será uma realidade acessível a todos.
+      </p>
+      <p>
+      Na <strong>Bridge Salutis</strong>, os nossos <strong>valores</strong> fundamentais são a <strong>inovação</strong>, <strong>transparência</strong>, a <strong>proatividade</strong>, <strong>humildade</strong> e o <strong>respeito</strong>.
+      A proatividade é central para a nossa abordagem: estamos sempre dois passos à frente, antecipando necessidades e criando soluções antes que os problemas surjam. Agir de forma rápida e eficiente é essencial para garantir o sucesso dos nossos clientes.
+      A transparência e a humildade mantêm-nos conectados às reais necessidades dos nossos parceiros e clientes. Embora sejamos especialistas nas nossas áreas, acreditamos que há sempre espaço para aprender, ouvir e evoluir, respeitando as opiniões e experiências de todos.
+      Valorizamos profundamente <strong>o direito à liberdade de consciência</strong>, reconhecendo que o respeito pela diversidade de opiniões e valores é crucial para criar um ambiente de trabalho <strong>justo</strong> e <strong>inclusivo</strong>.
+      </p>
+      <p>
+      Estamos comprometidos em oferecer <strong>soluções personalizadas</strong>, garantindo que cada cliente tenha acesso aos melhores recursos para o seu crescimento. Mantemos também um <strong>compromisso contínuo</strong> com o <strong>desenvolvimento e adaptação às mudanças no mercado</strong>, pois acreditamos que a evolução constante é essencial para assegurar que Portugal esteja na linha da frente dos avanços tecnológicos e de saúde.
+      Estamos aqui para construir as <strong>pontes essenciais</strong> e imprescindíveis, que permitirão que  empresas e StartUps de Enfermagem prosperem, assegurando que o futuro da saúde seja brilhante, inovador e acessível a todos.
+      </p>
+        
       </div>
     </section>
   );
